70815d833de33a4db431c50078279813840f336a
juliaholz110 <jujuholz9@gmail.com>
2025-06-08 22:37:30 -0300

    Inserindo Form Alert, js e css

---
+++ trabalhoweb/js/formAlert.js
@@ -0,0 +1,7 @@
+document.getElementById('form').addEventListener('submit', function (event){
+     //executa a função após o a ação do submit
+     event.preventDefault()
+     //previnir o comportamento padrão de um formulario
+     //impede o recarregamento da pagina
+     alert("Página em construção!")
+});
